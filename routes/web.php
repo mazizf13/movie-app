ed8fee437149e3e966b75b21c56f089309e9eb72
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-05-24 23:39:00 +0700

    feat: API Usage & HTTP Client

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
-<?php
+<?php       
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MoviesController;
 
-Route::view('/', 'index');
-Route::view('/movie', 'show');
+Route::get('/', [MoviesController::class, 'index'])->name('movies.index');
+Route::get('/movies/{movie}', [MoviesController::class, 'show'])->name('movies.show');
+ 
