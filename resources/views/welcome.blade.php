5f35a73ead214894458605bd702aa2cd3eb8a2b3
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-05-24 07:38:32 +0700

    feat: index & detail page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,8 +19,12 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
+         @vite('resources/css/app.css')
     </head>
-    <body class="antialiased">
+    <body class="antialiased font-sans" >
+        <h1 class="text-3xl font-bold underline text-orange-500 ">
+            Hello worlddd!
+          </h1>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
