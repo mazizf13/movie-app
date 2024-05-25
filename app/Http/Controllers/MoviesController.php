df36cde8ffdca04d63b450dfd1ab8582b0e4a3d9
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-05-25 20:22:19 +0700

    feat: refactoring to view models

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\ViewModels\MoviesViewModel;
+use App\ViewModels\MovieViewModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
@@ -23,21 +25,17 @@ class MoviesController extends Controller
                 ->get('https://api.themoviedb.org/3/movie/now_playing')
                 ->json()['results'];
         
-            $genresArray = Http::withToken(config('services.tmdb.token'))
+            $genres = Http::withToken(config('services.tmdb.token'))
                 ->get('https://api.themoviedb.org/3/genre/movie/list')
                 ->json()['genres'];
 
-            $genres = collect($genresArray)->mapWithKeys(function($genre) {
-                return [$genre['id'] => $genre['name']];
-            });
+            $viewModel = new MoviesViewModel (
+                $popularMovies,
+                $nowPlayingMovies,
+                $genres,
+            );
 
-            // dump($nowPlayingMovies);
-
-            return view('index', [
-                'popularMovies' => $popularMovies,
-                'nowPlayingMovies' => $nowPlayingMovies,
-                'genres' => $genres,
-            ]);
+            return view('index', $viewModel);
         }
 
     /**
@@ -74,11 +72,9 @@ class MoviesController extends Controller
                 ->get('https://api.themoviedb.org/3/movie/'.$id.'?append_to_response=credits,videos,images')
                 ->json();
 
-        // dump($movie);
+        $viewModel = new MovieViewModel($movie);
 
-        return view('show', [
-            'movie' => $movie
-        ]);
+        return view('show', $viewModel);
     }
 
     /**
